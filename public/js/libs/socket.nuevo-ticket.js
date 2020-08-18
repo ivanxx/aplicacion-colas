53a20c561b73556c912a1ea624f112bc9485a0a2
ivanxx <ivan@monodosis.com>
2020-08-18 21:09:24 +0200

    Primer commit - produccion

---
+++ public/js/libs/socket.nuevo-ticket.js
@@ -0,0 +1,22 @@
+// Comando para establecer la conexión con el servidor
+var socket = io();
+var label = $('#lblNuevoTicket');
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', function() {
+    console.log('Desconectado al servidor');
+});
+
+socket.on('estadoActual', function(resp) {
+    console.log(resp.actual);
+    label.text(resp.actual);
+});
+
+$('button').on('click', function() {
+    socket.emit('siguienteTicket', null, function(siguienteTicket) {
+        label.text(siguienteTicket);
+
+    })
+});
